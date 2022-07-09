926d7863b3fe769807e19c0bf27167eee120f706
Emil Shpeklord <emil42661@gmail.com>
2022-07-09 15:55:41 +0300

    main scene is done

---
+++ BeerViper/BeerScene/BeerCollectionViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  BeerCollectionViewCell.swift
+//  BeerViper
+//
+//  Created by Emil Shpeklord on 09.07.2022.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+final class BeerCollectionViewCell: UICollectionViewCell {
+    private lazy var itemStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fill
+        return stack
+    }()
+
+    private lazy var beerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "text"
+        label.font = .boldSystemFont(ofSize: 28)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+
+    //MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setUpUI()
+    }
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUpUI() {
+        contentView.addSubview(itemStack)
+        itemStack.addArrangedSubview(beerImageView)
+        itemStack.addArrangedSubview(label)
+
+        backgroundColor = .clear
+        layer.cornerRadius = 12
+        layer.shadowOpacity = 0.3
+        layer.shadowRadius = 4
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowColor = UIColor.systemGray.cgColor
+
+        contentView.layer.cornerRadius = 12
+        contentView.clipsToBounds = true
+
+        itemStack.snp.makeConstraints({ make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        })
+    }
+
+    func setUpCellData(urlString: String, name: String) {
+        guard let url = URL(string: urlString) else { return }
+        beerImageView.kf.setImage(with: url)
+        label.text = name
+    }
+}
--- BeerViper/BeerScene/BeerViewController.swift
+++ BeerViper/BeerScene/BeerViewController.swift
@@ -9,15 +9,57 @@ import UIKit
 
 final class BeerViewController: UIViewController, BeerViewProtocol {
     var presenter: BeerPresenterProtocol!
-    
+
+    private let reusableIdentifier = "BeerCell"
+
+    private var beerList: [Beer.InitForm.ViewModel] = []
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let width = UIScreen.main.bounds.width
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 20, bottom: 16, right: 20)
+        layout.itemSize = CGSize(width: (width - 60) , height: width)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 50
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(BeerCollectionViewCell.self, forCellWithReuseIdentifier: reusableIdentifier)
+
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        setUpUI()
         presenter.getData()
     }
 
     func presentData(vm: [Beer.InitForm.ViewModel]) {
-        print(vm)
+        beerList = vm
+        collectionView.reloadData()
+    }
+
+    private func setUpUI() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints({ make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        })
+    }
+}
+
+extension BeerViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return beerList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusableIdentifier, for: indexPath) as! BeerCollectionViewCell
+        let beer = beerList[indexPath.row]
+        cell.setUpCellData(urlString: beer.imageURL, name: beer.name)
+
+        return cell
     }
 }
